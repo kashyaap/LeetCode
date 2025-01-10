64ce3652161f5ad96a5270947b925c7852cb0eec
Abhinav Kashyap <32427623+kashyaap@users.noreply.github.com>
2025-01-11 01:18:57 +0530

    Update 26. Remove Duplicates from Sorted Array.py

--- 26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array.py
@@ -1,16 +1,40 @@
 class Solution(object):
+    # def removeDuplicates(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     counter = 1
+    #     val = nums[0]
+    #     while(counter < len(nums)):
+    #         if(nums[counter] == val):
+    #             nums.pop(counter)
+    #         else:
+    #             val = nums[counter]
+    #             counter += 1
+              
+    #     print len(nums), nums
+
+    # Faster than POP solution
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        counter = 1
+        pointer = 0
+        k =1
         val = nums[0]
-        while(counter < len(nums)):
-            if(nums[counter] == val):
-                nums.pop(counter)
+
+        while(pointer < len(nums)):
+            if(val == nums[pointer]):
+                pointer += 1
             else:
-                val = nums[counter]
-                counter += 1
-              
-        print len(nums), nums
+                nums[k] = nums[pointer]
+                val = nums[pointer]
+                pointer += 1
+                k +=1 
+                
+        nums = nums[0:k]
+
+        return k
+
